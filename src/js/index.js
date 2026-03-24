97c21608abb996f6255ea13ffcfef361432c1053
Darlon <darloncosta050@gmail.com>
2026-03-24 20:08:50 -0300

    star Rating

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const btnSearch = document.getElementById("btnSearch");
 const btnWatchLater = document.getElementById("btnWatchLater");
 const movieSelected = document.getElementById("movieSelected");
 
+const stars = new StarRating(".star-rating");
 const key = "a766a8b2";
 let currentMovie = null;
 let watchLater = [];
@@ -40,25 +41,61 @@ async function getFilms() {
 }
 
 function addWatchLater() {
-    watchLater.push(currentMovie)
-    console.log(watchLater)
+  watchLater.push(currentMovie);
+  console.log(currentMovie);
+  Swal.fire("Adicionado em assistir mais tarde");
 }
 
 function showMovie() {
-    const render = watchLater.map((movie) => {
-        return `
+  const render = watchLater.map((movie) => {
+    return `
             <div>
-                <h2>${movie.Title} (${movie.Year})</h2>
+                <div class="flex justify-between">
+                    <h2>${movie.Title} (${movie.Year})</h2>
+                    <button class="bg-red-600" onclick="removeMovie(${movie.imdbID})">X</button>
+                </div>
                 <img src="${movie.Poster}" alt="${movie.Title}">
                 <p><strong>Diretor:</strong> ${movie.Director}</p>
                 <p><strong>Gênero:</strong> ${movie.Genre}</p>
-                <p><strong>Nota:</strong> ${movie.imdbRating}</p>
+                <select class="star-rating" data-imdb-id="${movie.imdbID}">
+                    <option value="">Avaliar</option>
+                    <option value="5">5 estrelas</option>
+                    <option value="4">4 estrelas</option>
+                    <option value="3">3 estrelas</option>
+                    <option value="2">2 estrelas</option>
+                    <option value="1">1 estrela</option>
+                </select>
             </div>
             `;
-    })
+  });
 
-    movieSelected.innerHTML = render.join("")
+  movieSelected.innerHTML = render.join("");
+  initStarRating()
 }
 
-btnWatchLater.addEventListener('click', showMovie)
+function initStarRating() {
+  if (typeof StarRating !== "undefined") {
+    new StarRating(".star-rating", {
+      classNames: {
+        active: "gl-active",
+        base: "gl-star-rating",
+        selected: "gl-selected",
+      },
+      clearable: true, 
+      maxStars: 5, 
+      prebuilt: false,
+      stars: null,
+      tooltip: "Clique para avaliar", 
+    });
+  }
+}
+
+function removeMovie(imdbID) {
+  watchLater = watchLater.filter((movie) => {
+    return movie.imdID !== imdbID;
+  });
+  showMovie();
+}
+
+btnWatchLater.addEventListener("click", showMovie);
 btnSearch.addEventListener("click", getFilms);
