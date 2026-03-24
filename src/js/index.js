4ce280d6df824ba786e1c437f9251f5e0fdb879d
Darlon <darloncosta050@gmail.com>
2026-03-24 18:16:37 -0300

    att

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,41 @@
-const result = document.getElementById('result')
-const searchFilm = document.getElementById('searchFilm')
-const btnSearch = document.getElementById('btnSearch')
+const result = document.getElementById("result");
+const searchFilm = document.getElementById("searchFilm");
+const btnSearch = document.getElementById("btnSearch");
 
-const key = "a766a8b2"
+const key = "a766a8b2";
+let favoritesMovies = []
+
 
 async function getFilms() {
-    const film = searchFilm.value.trim()
+  if (searchFilm.value === "") {
+    alert("Informe o nome do filme");
+    return;
+  }
 
-    try {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${key}&t=${film}`)
-        const data = await response.json()
+  const film = searchFilm.value.trim();
 
-        console.log(data)
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${key}&s=${film}`,
+    );
+    const data = await response.json();
 
-        result.innerHTML = `
-            <div>
-                <h2>${data.Title} (${data.Year})</h2>
-                <img src="${data.Poster}" alt="${data.Title}">
-                <p><strong>Diretor:</strong> ${data.Director}</p>
-                <p><strong>Gênero:</strong> ${data.Genre}</p>
-                <p><strong>Nota:</strong> ${data.imdbRating}</p>
-                <p><strong>Sinopse:</strong> ${data.Plot}</p>
+    console.log(data);
+
+    const filmesHTML = data.Search.map(
+      (films) => `
+            <div class="flex items-center flex-col  border gap-4 rounded-lg">
+                <h2 class="text-xl font-bold">${films.Title} (${films.Year})</h2>
+                <img src="${films.Poster !== "N/A" ? films.Poster : "https://via.placeholder.com/300x450?text=Sem+Imagem"}" 
+                     alt="${films.Title}" class="w-40">
             </div>
-            `
-    } catch (error) {
-        console.log(error)
-    }
+        `,
+    ).join("");
+
+    result.innerHTML = filmesHTML;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-btnSearch.addEventListener('click', getFilms)
+btnSearch.addEventListener("click", getFilms);
